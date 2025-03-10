a1c4a6745c4a07958d5166a59b21c1dad9ec6077
aakanksha <akhandwaha@mta.ca>
2025-03-10 02:02:29 -0400

    added content!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='Header-Box'>
+          <div>
+            <h2 className='Subtitle'>
+              Hi there!
+            </h2>
+            <h1 className='Title'>
+              I'm Aakanksha.
+            </h1>
+            <p>
+              I'm a first-year MMath student at the David R. Cheriton School of Computer Science at the University of Waterloo.
+              I'm supervised by <a className="ExternalLink" href="http://edithlaw.ca" target="_blank">Dr. Edith Law</a> in the <a className="ExternalLink" href="http://hci.cs.uwaterloo.ca" target="_blank">human-computer interaction (HCI) lab</a>.
+            </p>
+            <div>
+              <a className="InternalLink" href="">Work</a>
+              <a className="InternalLink" href="">Projects</a>
+              <a className="InternalLink" href="./CV.pdf" target='_blank'>CV</a>
+            </div>
+          </div>
+          <img src={"./aakanksha.jpeg"} className="App-logo" alt="A picture of Aakanksha standing on a big rock" />
+        </div>
+
       </header>
     </div>
   );
